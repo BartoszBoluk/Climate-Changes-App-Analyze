278a88eb1b880912001aab65f96f878e5755a100
Bartosz Boluk <79060918+BartoszBoluk@users.noreply.github.com>
2022-12-04 22:28:30 +0100

    Add files via upload

--- src/main/java/com/example/climatechangesanalysis/AnalyzeClimateActivity.java
+++ src/main/java/com/example/climatechangesanalysis/AnalyzeClimateActivity.java
@@ -3,15 +3,11 @@ package com.example.climatechangesanalysis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.Dialog;
 import android.app.DownloadManager;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.webkit.URLUtil;
 import android.widget.Button;
@@ -21,32 +17,25 @@ import android.widget.Toast;
 import com.opencsv.CSVReader;
 
 import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.CookieManager;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class AnalyzeClimateActivity extends AppCompatActivity {
 
-    private TextView test;
-    private Button buttonStart;
-
-    // Progress Dialog
-    private ProgressDialog pDialog;
-    public static final int progress_bar_type = 0;
-
-    // File url to download
-    private static String file_url = "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/klimat/2001/2001_m_k.zip";
+    private TextView infoTextView, mClimateResultFirstDate1, mClimateResultFirstDate2, mClimateResultFirstDate3,
+            mClimateResultFirstDate4, mClimateResultFirstDate5, mClimateResultFirstDate6,
+            mClimateResultFirstDate7, mClimateResultFirstDate8, mClimateResultFirstDate9,
+            mClimateResultFirstDate10, mClimateResultFirstDate11, mClimateResultFirstDate12;
+    private Button mButtonStart;
+    private String cityName = "Pszczyna";
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -54,10 +43,23 @@ public class AnalyzeClimateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analyze_climate);
 
-        test = findViewById(R.id.textView2);
-        buttonStart = findViewById(R.id.buttonStartAnalyze);
+        infoTextView = findViewById(R.id.textView2);
+        mButtonStart = findViewById(R.id.buttonStartAnalyze);
+        mClimateResultFirstDate1 = findViewById(R.id.climateResultFirstDate1);
+        mClimateResultFirstDate2 = findViewById(R.id.climateResultFirstDate2);
+        mClimateResultFirstDate3 = findViewById(R.id.climateResultFirstDate3);
+        mClimateResultFirstDate4 = findViewById(R.id.climateResultFirstDate4);
+        mClimateResultFirstDate5 = findViewById(R.id.climateResultFirstDate5);
+        mClimateResultFirstDate6 = findViewById(R.id.climateResultFirstDate6);
+        mClimateResultFirstDate7 = findViewById(R.id.climateResultFirstDate7);
+        mClimateResultFirstDate8 = findViewById(R.id.climateResultFirstDate8);
+        mClimateResultFirstDate9 = findViewById(R.id.climateResultFirstDate9);
+        mClimateResultFirstDate10 = findViewById(R.id.climateResultFirstDate10);
+        mClimateResultFirstDate11 = findViewById(R.id.climateResultFirstDate11);
+        mClimateResultFirstDate12 = findViewById(R.id.climateResultFirstDate12);
 
-        test.setText("XD");
+        infoTextView.setText("XD");
+        cityName = cityName.toUpperCase();
         String URL = "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/klimat/2001/2001_m_k.zip";
 
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(URL));
@@ -79,27 +81,66 @@ public class AnalyzeClimateActivity extends AppCompatActivity {
             System.out.println("Sadge");
         }
 
-        buttonStart.setOnClickListener(new View.OnClickListener() {
+        mButtonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
                     unzip(file, new File("/sdcard/Download"));
-                    test.setText("Rozpakowano dane");
+                    infoTextView.setText("Rozpakowano dane");
 
                     try {
                         File csvfile = new File("/sdcard/Download/k_m_t_2001.csv");
                         CSVReader reader = new CSVReader(new FileReader(csvfile.getAbsolutePath()));
-                        String[] nextLine;
-                        while ((nextLine = reader.readNext()) != null) {
-                            // nextLine[] is an array of values from the line
-                            //System.out.println(nextLine[0] + nextLine[1] + "etc...");
-                            System.out.println(nextLine[2]);
-                            break;
+                        List<String[]> nextLine2 = reader.readAll();
 
-//                            nextLine[0] = String.valueOf(reader.readAll());
+                        ArrayList<String> dataList = new ArrayList<>();
+
+                        for (int i = 0; i < nextLine2.size(); i++) {
+                            String[] strings = nextLine2.get(i);
+                            for (int j = 0; j < strings.length; j++) {
+                                //System.out.println(strings[j] + " ");
+                                dataList.add(strings[j]);
+                            }
+                            //System.out.println();
                         }
 
+                        System.out.println("XD: " + nextLine2.size());
+                        System.out.println(cityName + ": " + dataList.size());
 
+                        String[] nextLine;
+                        float average = 0;
+
+
+                        for (int i = 0; i < dataList.size(); i++) {
+                            if (dataList.get(i).equals(cityName)) {
+                                average += Float.parseFloat(dataList.get(i+3));
+                            }
+                        }
+
+                        System.out.println("Średnia: " + average / 12);
+
+
+//                        while ((nextLine = reader.readNext()) != null) {
+//                            // nextLine[] is an array of values from the line
+//                            //System.out.println(nextLine[0] + nextLine[1] + "etc...");
+//                            System.out.println(nextLine[0]);
+//                            System.out.println(nextLine[1]);
+//                            System.out.println(nextLine[2]);
+//                            System.out.println(nextLine[3]);
+//                            System.out.println(nextLine[4]);
+//                            System.out.println(nextLine[5]);
+//                            System.out.println(nextLine[6]);
+//                            System.out.println(nextLine[7]);
+//                            System.out.println(nextLine[8]);
+//                            System.out.println(nextLine[9]);
+//                            System.out.println(nextLine[10]);
+//                            System.out.println(nextLine[11]);
+//                            System.out.println(nextLine[12]);
+//
+//                            break;
+//
+////                            nextLine[0] = String.valueOf(reader.readAll());
+//                        }
 
 //                        String[] splitStr = nextLine[0].split("\\,");
 //
@@ -119,68 +160,6 @@ public class AnalyzeClimateActivity extends AppCompatActivity {
         });
 
 
-
-//        String path = "/" + Environment.DIRECTORY_DCIM + "/" + title;
-//        System.out.println(path);
-//
-//        File file = new File(Environment.DIRECTORY_DOWNLOADS + "/" + title);
-//
-//        unpackZip(Environment.getExternalStorageDirectory().getAbsolutePath() + "/test/", title);
-//
-//        System.out.println(file.getAbsolutePath());
-
-//        try {
-//            unzip(file, new File(Environment.DIRECTORY_DOWNLOADS));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            System.out.println("XDDDDD");
-//        }
-
-//        unpackZip(path, title);
-
-    }
-
-    private boolean unpackZip(String path, String zipname)
-    {
-        InputStream is;
-        ZipInputStream zis;
-        try
-        {
-            is = new FileInputStream(path + zipname);
-            zis = new ZipInputStream(new BufferedInputStream(is));
-            ZipEntry ze;
-
-            while((ze = zis.getNextEntry()) != null)
-            {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                byte[] buffer = new byte[1024];
-                int count;
-
-                String filename = ze.getName();
-                FileOutputStream fout = new FileOutputStream(path + filename);
-
-                // reading and writing
-                while((count = zis.read(buffer)) != -1)
-                {
-                    baos.write(buffer, 0, count);
-                    byte[] bytes = baos.toByteArray();
-                    fout.write(bytes);
-                    baos.reset();
-                }
-
-                fout.close();
-                zis.closeEntry();
-            }
-
-            zis.close();
-        }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-            return false;
-        }
-
-        return true;
     }
 
     public static void unzip(File zipFile, File targetDirectory) throws IOException {
